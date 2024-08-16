dd9a74de013ef18a7a70ad279463b1f3e9c89772
Rajiv-Chaurasiya <rajiv84ia@gmail.com>
2024-08-16 18:02:42 +0530

    file modified

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        API:'http://65.2.191.9:5000/api/v1'
+        API:'http://13.235.185.253:8090/api/v1'
     },
     images:{
         domains:['res.cloudinary.com']
